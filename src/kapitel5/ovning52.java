f94cce916e50ba03a18436fbfc66a50d90c803f0
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2020-10-16 11:19:32 +0200

    övning 5.2 klar

--- src/kapitel5/ovning52.java
+++ src/kapitel5/ovning52.java
@@ -3,17 +3,17 @@ package kapitel5;
 import javax.swing.*;
 
 /*
-Loop som räknar från 1^2 till n^2 och summerar alla nummer.
+Beräknar den harmoniska serien.
 Filip Andersson 2020-10-16
 */
 
-public class ovning51 {
+public class ovning52 {
     public static void main (String[] args) {
         int n = Integer.parseInt(JOptionPane.showInputDialog("n?"));
-        int summa = 0;
-        int k = 1;
-        while (k <= n*n) {
-            summa = summa + k*k;
+        double summa = 0;
+        double k = 1;
+        while (k <= n) {
+            summa = summa + 1/k;
             k = k + 1;
         }
         JOptionPane.showMessageDialog(null, "Summan blir " + summa);
