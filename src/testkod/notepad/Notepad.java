b8a60dfcb459ea536a8ece7c502bf524ca835e9f
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-02-16 11:14:43 +0100

    feat: readfile funkar

--- src/testkod/notepad/Notepad.java
+++ src/testkod/notepad/Notepad.java
@@ -3,10 +3,18 @@ package testkod.notepad;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Notepad {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JScrollPane scrollPane1;
+    private JMenuBar menuBar;
 
     public Notepad() {
         menuBar = new JMenuBar();
@@ -14,30 +22,57 @@ public class Notepad {
         JMenuItem menuItem1 = new JMenuItem("New");
         menu1.add(menuItem1);
         JMenuItem menuItem2 = new JMenuItem("Save");
-        menuItem2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        menuItem2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
         menu1.add(menuItem2);
-        JMenuItem menuItem3 = new JMenuItem("Load");
+        JMenuItem menuItem3 = new JMenuItem("Open...");
         menu1.add(menuItem3);
         menuBar.add(menu1);
-        menuItem1.addActionListener(new ActionListener() {
+        menuItem1.addActionListener(new ActionListener() {  //New file
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("new");
                 textArea1.setText("");
             }
         });
-        menuItem2.addActionListener(new ActionListener() {
+        menuItem2.addActionListener(new ActionListener() {  //Save file
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("save");
             }
         });
-        menuItem3.addActionListener(new ActionListener() {
+        menuItem3.addActionListener(new ActionListener() {  //Open file
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("load");
+                readFile();
             }
         });
+    }
+
+    private void readFile() {
+        JFileChooser fc = new JFileChooser();
+        int result = fc.showOpenDialog(null);
+        if (result!=JFileChooser.APPROVE_OPTION){
+            System.out.println("ingen fil valdes");
+            System.exit(0);
+        }
+        String filename = fc.getSelectedFile().getAbsolutePath();
+        FileReader fr = null;
+        textArea1.setText("");
+        try {
+            fr = new FileReader(filename);
+        } catch (FileNotFoundException g) {
+            g.printStackTrace();
+        }
+        BufferedReader inFile = new BufferedReader(fr);
+
+        String line;
+        try {
+            while ((line = inFile.readLine() ) != null) {
+                textArea1.append(line+"\n");
+            }
+            inFile.close();
+        } catch (IOException f) {
+            f.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -49,8 +84,4 @@ public class Notepad {
         frame.pack();
         frame.setVisible(true);
     }
-
-    private JPanel panel1;
-    private JTextArea textArea1;
-    private JMenuBar menuBar;
 }
